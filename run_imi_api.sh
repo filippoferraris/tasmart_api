ed79961f49d9a8214a6bc57bc780aed0f218041b
EC2 Default User <ec2-user@ip-172-31-43-224.ec2.internal>
2023-09-02 20:20:55 +0000

    updated bash command

--- run_imi_api.sh
+++ run_imi_api.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-/home/ec2-user/tasmart_api
+
+# Change the working directory to /home/ec2-user/tasmart_api
+cd /home/ec2-user/tasmart_api
+
+# Execute the Python script
 python3 imi_api.py
